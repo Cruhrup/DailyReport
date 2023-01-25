d7717771e0f3268ba94c94251990e8af64fa7e76
Chris Ruhrup <79858481+Cruhrup@users.noreply.github.com>
2023-01-25 12:35:55 -0600

    Added Outlook emailer
    added the O365 emailer section that works via Azure and O365 module in python-O365

--- Devicelist.py
+++ Devicelist.py
@@ -1,11 +1,7 @@
 from panos.firewall import Firewall
 from panos.panorama import Panorama
 from okta.client import Client as OktaClient
-from okta.exceptions import OktaAPIException
-
-'''
-I know hardcoding API keys is bad practice, working on a better solution
-'''
+from O365 import Account
 
 #Firewalls
 firewall01 = Firewall("FW_IP_HERE", api_key="MY_API_KEY_HERE")
@@ -18,7 +14,7 @@ panorama01 = Panorama("PANORAMA_IP_HERE", api_key="MY_API_KEY_HERE")
 #Instantiating with a Python dictionary in the constructor
 config = {
     'orgUrl': 'https://YOUR_OKTA_ORG_HERE.okta.com',
-    #crbot token in Okta prod admin expires 30 days without usage
+    #Token expires 30 days without usage
     'token': 'MY_API_TOKEN_HERE'
 }
 okta_client = OktaClient(config)
@@ -32,3 +28,14 @@ OKTAAGENT02 = "HOSTNAME_OR_IP_HERE"
 fw_list = [firewall01, firewall02]
 panorama_list = [panorama01]
 okta_agent_list = [OKTAAGENT01, OKTAAGENT02]
+
+#EMAILER
+#grabbing delegated permissions here via scope helper
+scopes = ['message_send', 'basic', 'message_send_shared']
+credentials = ('Application(Client)_ID_HERE', 'Client_Secret_Value_HERE')
+account = Account(credentials, tenant_id='TENANT_ID_HERE', scopes=scopes)
+
+''' Uncomment below 'if' statement to authenticate if no auth token in working dir or auth token expired
+if account.authenticate():
+    print('Authenticated!')
+'''
